992a5309739a17697f00fc35f7085f69b42c57e4
Matheus <matheuscorrea9@gmail.com>
2026-05-13 09:40:21 -0300

    Read dir and output

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
 use clap::Parser;
-use std::path::PathBuf;
+use owo_colors::OwoColorize;
+use std::{fs, path::{Path, PathBuf}};
 
 #[derive(Debug, Parser)]
+#[command(version, about, long_about = "Catholic Driven CLI")]
 struct Cli {
     path: Option<PathBuf>,
 }
 
 fn main() {
     let cli = Cli::parse();
-    println!("{cli:?}");
+
+    let path = cli.path.unwrap_or(PathBuf::from("."));
+
+    if let Ok(does_exist) = fs::exists(&path)  {
+        if does_exist{
+            for file in get_files(&path) {
+                println!("{}", file)
+            }
+        }else {
+            println!("{}", "Path does not exist".red())
+        }
+    } else{
+        println!("{}", "error reading directory".red())
+    }
+}
+
+
+fn get_files(path: &Path) -> Vec<String> {
+    let mut data = Vec::default();
+    if let Ok(read_dir) = fs::read_dir(path) {
+        for entry in read_dir {
+            if let Ok(file) = entry {
+                data.push(
+                    file.file_name()
+                        .into_string()
+                        .unwrap_or("unknown name".into()),
+                )
+            }
+        }
+    }
+    data
 }
